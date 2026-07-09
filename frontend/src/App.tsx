bc1483dfb2498c277c3ea8bb247b7e7e7ae58011
pshingsl <w455ok@gmail.com>
2026-07-09 19:41:47 +0900

    feat: reservation. payment 연결

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,10 @@ import ConcertListPage from './pages/concert/ConcertListPage';
 import ConcertDetailPage from './pages/concert/ConcertDetailPage';
 import PrivateRoute from './components/PrivateRoute';
 import SeatPage from './pages/seat/SeatPage';
+import ReservationPreviewPage from './pages/reservation/ReservationPreviewPage';
+import PaymentPage from './pages/payment/PaymentPage';
+import PaymentCompletePage from './pages/payment/PaymentCompletePage';
+import PaymentFailPage from './pages/payment/PaymentFailPage';
 
 function App() {
   return (
@@ -19,12 +23,17 @@ function App() {
         {/* 공개 라우트 */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/payment/complete" element={<PaymentCompletePage />} />
+        <Route path="/payment/fail" element={<PaymentFailPage />} />
 
         {/* 인증 필요 라우트 */}
         <Route element={<PrivateRoute />}>
           <Route path="/mypage" element={<MyPage />} />
           <Route path="/seats/:scheduleId" element={<SeatPage />} />
+          <Route path="/reservations/preview" element={<ReservationPreviewPage />} />
+          <Route path="/payment" element={<PaymentPage />} />
         </Route>
+
         {/* 기본경로 */}
         <Route path="/" element={<LoginPage />} />
       </Routes>
